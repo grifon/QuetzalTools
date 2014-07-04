42d4f9c26beac062990ef4e178d651f2ca6472b2
Grigory Bychek <gbychek@gmail.com>
2014-07-04 12:04:50 +0400

    Создан bootstrap с автозагрузчиком классов

---
+++ bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+spl_autoload_register(
+	function($class) {
+		$catalogs = array('Configuration', 'Helper');
+
+		foreach ($catalogs as $catalog) {
+			$fileName = sprintf('%s/%s.php', $catalog, $class);
+
+			if (file_exists($fileName)) {
+				require_once $fileName;
+			}
+		}
+	}
+);

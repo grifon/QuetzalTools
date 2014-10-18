d67f17aa580cc20df1a7fa01fa4d29ba7a7ba2ce
Grigory Bychek <gbychek@gmail.com>
2014-10-19 00:40:10 +0400

    Добавлен абстрактный класс модели

---
+++ Data/Model.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Quetzal\Data;
+
+/**
+ * Абстрактный класс модели
+ *
+ * Class Model
+ *
+ * @author Grigory Bychek <gbychek@gmail.com>
+ *
+ * @package Quetzal\Data
+ */
+abstract class Model
+{
+}

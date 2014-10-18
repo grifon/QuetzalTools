d72fb202d2ad2a8eee877cff958c9cae08162c26
Grigory Bychek <gbychek@gmail.com>
2014-10-19 00:08:08 +0400

    Добавлен CommonConfiguration

---
+++ Environment/Configuration/CommonConfiguration.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Quetzal\Environment\Configuration;
+
+/**
+ * Класс общей конфигурации для всех сайтов
+ *
+ * Class CommonConfiguration
+ *
+ * @author Grigory Bychek <gbychek@gmail.com>
+ */
+class CommonConfiguration
+{
+	/**
+	 * Хранимые параметры конфигурации
+	 *
+	 * @var array
+	 */
+	protected $params = array();
+
+	/**
+	 * @param array $config Набор конфигурационных параметров
+	 */
+	public function __construct(array $config = array())
+	{
+		$this->params = $config;
+	}
+
+	/**
+	 * Получает значение хранимого в конфигурации параметра
+	 *
+	 * @param string $key
+	 *
+	 * @return mixed
+	 */
+	public function get($key)
+	{
+		return isset($this->params[$key]) ? $this->params[$key] : null;
+	}
+
+	/**
+	 * Сохраняет значение параметра конфигурации
+	 *
+	 * @param mixed $key
+	 * @param mixed $value
+	 */
+	public function set($key, $value)
+	{
+		$this->params[$key] = $value;
+	}
+
+	/**
+	 * Получает всю хранимую конфигурацию
+	 *
+	 * @return array
+	 */
+	public function all()
+	{
+		return $this->params;
+	}
+}

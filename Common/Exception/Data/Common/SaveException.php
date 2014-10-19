72760901b47758e45a7c70a28de912efdc71231c
Grigory Bychek <gbychek@gmail.com>
2014-10-19 16:39:20 +0400

    Добавлено исключение процесса сохранения объекта

---
+++ Common/Exception/Data/Common/SaveException.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Quetzal\Common\Exception\Data\Common;
+
+use Quetzal\Common\Exception;
+
+/**
+ * Исключение процесса сохранения объекта
+ *
+ * Class SaveException
+ *
+ * @author Grigory Bychek <gbychek@gmail.com>
+ *
+ * @package Quetzal\Common\Exception\Data\Common
+ */
+class SaveException extends Exception
+{
+	/**
+	 * @var int
+	 */
+	protected $id;
+
+	/**
+	 * @param string $message
+	 * @param int $id
+	 * @param int $code
+	 * @param Exception $previous
+	 */
+	public function __construct($message, $id = null, $code = 0, Exception $previous = null)
+	{
+		parent::__construct($message, $code, $previous);
+
+		$this->id = $id;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+} 

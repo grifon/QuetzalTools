ae02e062edf85240f8bd62667fb7dc1d8a500fd7
Grigory Bychek <gbychek@gmail.com>
2014-10-19 16:30:19 +0400

    Добавлен интерфейс менеджера объектов

---
+++ Data/Common/Persistence/ObjectManagerInterface.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Quetzal\Data\Common\Persistence;
+
+use Quetzal\Data\Common\Model;
+
+/**
+ * Интерфейс менеджера объектов хранилища
+ *
+ * Interface ObjectManagerInterface
+ *
+ * @author Grigory Bychek <gbychek@gmail.com>
+ *
+ * @package Quetzal\Data\Common\Persistence
+ */
+interface ObjectManagerInterface
+{
+	/**
+	 * Получает репозиторий текущего менеджера
+	 *
+	 * @return ObjectRepositoryInterface
+	 */
+	public function getRepository();
+
+	/**
+	 * Получает один элемент инфоблока по id
+	 *
+	 * @param int $id
+	 *
+	 * @return array|null
+	 */
+	public function find($id);
+
+	/**
+	 * Выбирает элементы по фильтру
+	 *
+	 * @param array $filter
+	 * @param array $orderBy
+	 *
+	 * @return array
+	 */
+	public function findBy(array $filter = array(), array $orderBy = array());
+
+	/**
+	 * Получает один элемент инфоблока по фильтру
+	 *
+	 * @param array $filter
+	 *
+	 * @return array|null
+	 */
+	public function findOneBy(array $filter = array());
+
+	/**
+	 * Сохранаяет элемент
+	 *
+	 * @param Model $item
+	 */
+	public function save(Model $item);
+
+	/**
+	 * Удаляет элемент
+	 *
+	 * @param int $id
+	 */
+	public function delete($id);
+}

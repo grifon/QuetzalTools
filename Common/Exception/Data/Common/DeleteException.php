6d3eb6e285fd7b89a36c7dd9847ca3f8d2458bf8
Grigory Bychek <gbychek@gmail.com>
2014-10-19 16:39:33 +0400

    Добавлено исключение процесса удаления объекта

---
+++ Common/Exception/Data/Common/DeleteException.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Quetzal\Common\Exception\Data\Common;
+
+use Quetzal\Common\Exception;
+
+/**
+ * Исключение процесса удаления объекта
+ *
+ * Class DeleteException
+ *
+ * @author Grigory Bychek <gbychek@gmail.com>
+ *
+ * @package Quetzal\Common\Exception\Data\Common
+ */
+class DeleteException extends Exception
+{
+	/**
+	 * @var int
+	 */
+	protected $id;
+
+	/**
+	 * @param string $message
+	 * @param int $id
+	 * @param int $code
+	 * @param Exception $previous
+	 */
+	public function __construct($message, $id, $code = 0, Exception $previous = null)
+	{
+		parent::__construct($message, $code, $previous);
+
+		$this->id = $id;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+} 

d3268d857c9ebc452471fec0e54a5fa780aad9da
Grigory Bychek <gbychek@gmail.com>
2014-10-29 17:53:21 +0300

    Добавлено исключение Exception/Data/Migration/MigrationException

---
+++ Exception/Data/Migration/MigrationException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Quetzal\Exception\Data\Migration;
+
+use Quetzal\Common\Exception;
+
+/**
+ * Исключение процесса выполнения миграции
+ *
+ * Class MigrationException
+ *
+ * @author Grigory Bychek <gbychek@gmail.com>
+ *
+ * @package Quetzal\Exception\Data\Migration
+ */
+class MigrationException extends Exception
+{
+}

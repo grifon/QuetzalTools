3a6d3e53bb276046ab41b86a4357437448f2d2b0
Grigory Bychek <gbychek@gmail.com>
2017-04-14 22:32:31 +0300

    Tools/CDN/PathGenerator Реализован простейший алгоритм генерации путей http://track.studioratio.ru/youtrack/issue/RTI-332

--- Tools/CDN/PathGenerator.php
+++ Tools/CDN/PathGenerator.php
@@ -34,6 +34,10 @@ class PathGenerator
 	 */
 	public function __construct(array $domains)
 	{
+		if (empty($domains)) {
+			throw new \InvalidArgumentException('Domains list should be not empty');
+		}
+
 		$this->domains = $domains;
 	}
 
@@ -46,6 +50,32 @@ class PathGenerator
 	 */
 	public function generateCDNUri($uri)
 	{
-		return '';
+		return sprintf('//%s%s', $this->getDomain(), $uri);
+	}
+
+	/**
+	 * Получает очередной домен, с которого должен отдаваться файл
+	 *
+	 * @return string
+	 */
+	protected function getDomain()
+	{
+		$domain = $this->domains[$this->index];
+
+		$this->incIndex();
+
+		return $domain;
+	}
+
+	/**
+	 * Циклически увеличивает индекс указателя списка доменов
+	 */
+	protected function incIndex()
+	{
+		if ($this->index == count($this->domains) - 1) {
+			$this->index = 0;
+		} else {
+			$this->index++;
+		}
 	}
 }
